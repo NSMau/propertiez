4d2585743ff9b411723d75999b4e818c4f748dfc
NSMau <mauriciopaternina@icloud.com>
2023-11-22 22:26:27 -0500

    add nodemon

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (_, res) => res.send('Hello World!'))
+app.get('/', (_, res) => res.send('Hello, world!'))
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`))
