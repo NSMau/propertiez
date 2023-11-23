ed58b5d564ef203e9e1d91745ce767627cae2e1a
NSMau <mauriciopaternina@icloud.com>
2023-11-23 09:56:22 -0500

    create get and post express routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,27 @@
 import express from 'express'
+import bodyParser from "body-parser";
+
+import listings from './listings'
 
 const app = express()
 const PORT = 3000
 
-app.get('/', (_, res) => res.send('Hello, world!'))
+// Middleware to parse JSON body
+app.use(bodyParser.json())
+
+app.get('/listings', (_, res) => res.send(listings))
+
+// curl -X POST http://localhost:3000/delete-listing -H 'Content-Type: application/json' -d '{"id": "001"}'
+app.post('/delete-listing', (req, res) => {
+    const { id } = req.body
+
+    for (const listing of listings) {
+        if (listing.id === id) {
+            return res.send(listings.splice(listings.indexOf(listing), 1))
+        }
+    }
+
+    return res.send('Failed to delete listing')
+})
 
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
---
+++ server/src/listings.ts
@@ -0,0 +1,71 @@
+interface Listing {
+    id: string;
+    title: string;
+    image: string;
+    address: string;
+    price: number;
+    numOfGuests: number;
+    numOfBeds: number;
+    numOfBaths: number;
+    rating: number;
+}
+
+const listings: Listing[] = [
+    {
+        id: "001",
+        title: "Title 1",
+        image: "https://via.placeholder.com/150",
+        address: "Address 1",
+        price: 100,
+        numOfGuests: 2,
+        numOfBeds: 1,
+        numOfBaths: 1,
+        rating: 5
+    },
+    {
+        id: "002",
+        title: "Title 2",
+        image: "https://via.placeholder.com/150",
+        address: "Address 2",
+        price: 200,
+        numOfGuests: 3,
+        numOfBeds: 2,
+        numOfBaths: 2,
+        rating: 4
+    },
+    {
+        id: "003",
+        title: "Title 3",
+        image: "https://via.placeholder.com/150",
+        address: "Address 3",
+        price: 300,
+        numOfGuests: 4,
+        numOfBeds: 3,
+        numOfBaths: 3,
+        rating: 3
+    },
+    {
+        id: "004",
+        title: "Title 4",
+        image: "https://via.placeholder.com/150",
+        address: "Address 4",
+        price: 400,
+        numOfGuests: 5,
+        numOfBeds: 4,
+        numOfBaths: 4,
+        rating: 2
+    },
+    {
+        id: "005",
+        title: "Title 5",
+        image: "https://via.placeholder.com/150",
+        address: "Address 5",
+        price: 500,
+        numOfGuests: 6,
+        numOfBeds: 5,
+        numOfBaths: 5,
+        rating: 1
+    }
+]
+
+export default listings
